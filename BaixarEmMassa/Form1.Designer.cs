b2f84ae89e38310cee09b605f5fda38f38539672
gabriel7lib <gabriel7ya@gmail.com>
2020-12-15 21:17:01 -0300

    Primeiro commit

---
+++ BaixarEmMassa/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace BaixarEmMassa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblNumArq = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 12);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(473, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Status:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(55, 46);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(148, 13);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Selecione o arquivo com links";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(411, 41);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 6;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Baixados:";
+            // 
+            // lblNumArq
+            // 
+            this.lblNumArq.AutoSize = true;
+            this.lblNumArq.Location = new System.Drawing.Point(350, 46);
+            this.lblNumArq.Name = "lblNumArq";
+            this.lblNumArq.Size = new System.Drawing.Size(13, 13);
+            this.lblNumArq.TabIndex = 8;
+            this.lblNumArq.Text = "0";
+            this.lblNumArq.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 89);
+            this.Controls.Add(this.lblNumArq);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Download em massa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblNumArq;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ BaixarEmMassa/Form1.cs
@@ -0,0 +1,76 @@
+﻿using AltoHttp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaixarEmMassa
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog openFile = new OpenFileDialog();
+        string linha = "";
+        int totalLinhas = 0;
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFile.FileName);
+                totalLinhas = File.ReadAllLines(openFile.FileName).Length;
+                while (linha != null)
+                {
+                    linha = sr.ReadLine();
+
+                    if (linha != null)
+                    {
+                        baixarFila(sender, e, linha);
+                    }
+                    else {
+                        lblStatus.Text = "Concluído";
+                    }
+                }
+            }
+        }
+
+
+
+        HttpDownloader httpDownloader;
+        
+        private void baixarFila(object sender, EventArgs e, String linha)
+        {
+            httpDownloader = new HttpDownloader(linha, $"{Application.StartupPath}\\{Path.GetFileName(linha)}");
+            httpDownloader.DownloadCompleted += HttpDownloader_DownloadCompleted;
+            httpDownloader.ProgressChanged += HttpDownloader_ProgressChanged;
+            httpDownloader.Start();
+        }
+
+        private void HttpDownloader_ProgressChanged(object sender, AltoHttp.ProgressChangedEventArgs e)
+        {
+            progressBar.Value = (int)e.Progress;
+        }
+
+        private void HttpDownloader_DownloadCompleted(object sender, EventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                //lblStatus.Text = "Pronto!";
+                lblNumArq.Text = (int.Parse(lblNumArq.Text) + 1).ToString();
+
+            });
+        }
+
+    }
+}
